b295dd193f4f7952f591b69820d9de4cf134e884
Sivaranjini27 <144334341+Sivaranjini27@users.noreply.github.com>
2023-09-21 21:51:22 +0530

    Add files via upload

--- main 7.py
+++ main 7.py
@@ -1,44 +1,18 @@
 
 
 
-class Bank:
-	def __init__(self):
-		self.total_amount = 0
-		self.name = ''
+def linearSearchProduct(productList, targetProduct):
+  indices = []
 
-	def welcome(self):
-		self.name = input('Welcome to your Bank Account. Please Enter your name: ')
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
 
-	def print_current_balance(self):
-		print('Your Current balance : {}'.format(self.total_amount))
-
-	def deposit(self):
-		self.total_amount += float(input('Hello {}, please enter amount to deposit: '.format(self.name)))
-		self.print_current_balance()
-
-	def withdraw(self):
-		amount_to_withdraw = float(input('Enter amount to withdraw: '))
-
-		if amount_to_withdraw > self.total_amount:
-			print('Insufficient Balance !!')
-		else:
-			self.total_amount -= amount_to_withdraw
-
-		self.print_current_balance()
+  return indices
 
 
-if __name__=="__main__":
-	bank = Bank()
-	bank.welcome()
-
-	while True:
-		input_value = int(input('Enter 1 to see your balance,\n2 to deposit\n3 to withdraw\n'))
-
-		if input_value == 1:
-			bank.print_current_balance()        
-		elif input_value == 2:
-			bank.deposit()
-		elif input_value == 3:
-			bank.withdraw()
-		else:
-			print('Please enter a valid input.')
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(products, target)
+print(result)
--- main 8.py
+++ main 8.py
@@ -1,21 +1,34 @@
 
 
-class Player:
-    def play(self):
-        print("The player is playing cricket.")
 
-class Batsman(Player):
-    def play(self):
-        print("The batsman is batting.")
+class Student:
 
-class Bowler(Player):
-    def play(self):
-        print("The bowler is bowling.")
+  def __init__(self, name, roll_number, cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
 
-# Creating objects of the Batsman and Bowler classes
-batsman = Batsman()
-bowler = Bowler()
 
-# Calling the play() method for each object
-batsman.play()
-bowler.play()
+def sort_students(student_list):
+  # Sort the list of students in descending order of CGPA
+  sorted_students = sorted(student_list,
+                           key=lambda student: student.cgpa,
+                           reverse=True)
+  # Syntax - lambda arg:exp
+  return sorted_students
+
+
+students = [
+    Student("Hari", "A123", 7.8),
+    Student("Srikanth", "A124", 8.9),
+    Student("Saumya", "A125", 9.1),
+    Student("Mahidhar", "A126", 9.9),
+]
+
+sorted_students = sort_students(students)
+
+# Print the sorted list of students
+for student in sorted_students:
+  print("Name: {}, Roll Number: {}, CGPA: {}".format(student.name,
+                                                     student.roll_number,
+                                                     student.cgpa))
